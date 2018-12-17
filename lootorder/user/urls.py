0a2276f5ba6f260673ce706efcaf02263fc6e07a
Brett Kinnamon <bdkinna@gmail.com>
2018-12-16 22:19:15 -0500

    Signup, login and logout.

--- lootorder/user/urls.py
+++ lootorder/user/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='user-login'),
+    path('', views.login_view, name='user-login'),
     path('signup/', views.signup, name='user-signup'),
+    path('logout/', views.logout_view, name='user-logout'),
 ]
--- lootorder/user/views.py
+++ lootorder/user/views.py
@@ -1,40 +1,50 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(requets.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request.user)
+            login(request, user)
+            messages.success(request, 'Account created')
             return redirect('lootlists-dashboard')
     else:
         form = UserCreationForm()
     context = {
         'title': 'Signup',
+        'submit': 'Signup',
         'form': form,
     }
     return render(request, 'user/forms.html', context)
 
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(requets.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request.user)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful')
             return redirect('lootlists-dashboard')
-    else:
-        form = AuthenticationForm()
+        else:
+            messages.error(request, 'Login error')
+    form = AuthenticationForm()
     context = {
         'title': 'Login',
+        'submit': 'Login',
         'form': form,
     }
     return render(request, 'user/forms.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Logout successful')
+    return redirect('lootlists-dashboard')
