e7f81cdc7482a443607babb38cc444057b51f4c9
Brett Kinnamon <bdkinna@gmail.com>
2018-12-24 14:56:53 -0500

    Django for Heroku deploy

--- lootorder/lootorder/settings.py
+++ lootorder/lootorder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from django.contrib.messages import constants as messages
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,6 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
