2a92758181945f31812710c0b5eabb0fa168ad20
Brett Kinnamon <bdkinna@gmail.com>
2018-12-26 13:02:05 -0500

    Adjust the colorscheme, responsive layout and favicon

--- lootlists/urls.py
+++ lootlists/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path(
+        'favicon.ico',
+        RedirectView.as_view(
+            url=staticfiles_storage.url('favicon.ico'),
+            permanent=False
+        ),
+        name='favicon'
+    ),
     path(
         '',
         views.landing,
